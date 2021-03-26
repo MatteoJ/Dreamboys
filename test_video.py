bb59ef31b753690fdd4b772bcbfacabed65f4dd2
CyrielVD <47987234+CyrielVD@users.noreply.github.com>
2021-03-26 23:50:55 +0100

    alphapose added

--- test_video.py
+++ test_video.py
@@ -1,15 +1,15 @@
 import cv2
+import torch
 
-cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
 while True:
     rval, frame = vc.read()
     cv2.imshow("preview", frame)
-    rval, frame = vc.read()
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    if key == 27:
         break
 
 cv2.destroyWindow("preview")
 vc.release()
+
