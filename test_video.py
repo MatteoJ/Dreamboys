d9c3d467525f0e892c00a60822b295cb5f965a8b
CyrielVD <47987234+CyrielVD@users.noreply.github.com>
2021-03-26 22:32:52 +0100

    opencv webcam test

---
+++ test_video.py
@@ -0,0 +1,15 @@
+import cv2
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+while True:
+    rval, frame = vc.read()
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+cv2.destroyWindow("preview")
+vc.release()
